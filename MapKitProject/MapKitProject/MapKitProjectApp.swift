78d6618840a0d05a9951571b5802b10807288079
Motis2004 <miotis2004@gmail.com>
2020-11-28 14:27:07 -0500

    First Commit

---
+++ MapKitProject/MapKitProject/MapKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitProjectApp.swift
+//  MapKitProject
+//
+//  Created by Ronald Joubert on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MapKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
